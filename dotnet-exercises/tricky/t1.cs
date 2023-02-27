0e5c681f07a28b5409aebd1d68f11292e190bd9e
mateuszfranke <mateuszfranke@icloud.com>
2023-02-27 19:35:24 +0100

    tricky q 1

---
+++ dotnet-exercises/tricky/t1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace dotnet_exercises.tricky
+{
+	public class t1
+	{
+		public async Task Execute() {
+
+
+            async Task DoSomething()
+            {
+                Thread.Sleep(1000);
+                await Task.Delay(1000);
+            }
+
+            var watch = Stopwatch.StartNew();
+            var t1 = DoSomething();
+            var t2 = DoSomething();
+            await Task.WhenAll(t1, t2);
+
+            Console.WriteLine($"It took {watch.ElapsedMilliseconds}");
+            Console.ReadKey();
+        }
+	}
+}
+
