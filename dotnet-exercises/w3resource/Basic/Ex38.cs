7f9fe36d6c43e859efb658f62ed23652306243b8
mateuszfranke <mateuszfranke@icloud.com>
2023-03-01 10:14:26 +0100

    exercise 38

---
+++ dotnet-exercises/w3resource/Basic/Ex38.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+Write a C# program to get a new string of two characters from a given string.
+The first and second character of the given string must be "P" and "H", so PHP will be "PH".
+*/
+namespace dotnet_exercises.w3resource.Basic
+{
+    public class Ex38 : IRunner
+    {
+        public void Run()
+        {
+            const string input = "PHP Tutorial";
+            if (input.Length >= 2)
+            {
+                if (input[0] == 'P' && input[1] == 'H') {
+
+                    Console.WriteLine(input[0].ToString() + input[1]);
+                }
+            }
+        }
+    }
+}
+
