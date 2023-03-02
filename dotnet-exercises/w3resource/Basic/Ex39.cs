49ce46dc4423a9346982cc329173f2a2cba279a4
mateuszfranke <mateuszfranke@icloud.com>
2023-03-02 09:29:15 +0100

    exercise 39

---
+++ dotnet-exercises/w3resource/Basic/Ex39.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*
+Write a C# program to find the largest and lowest values from three integer values. Go to the editor
+Test Data:
+Input first integer:
+15
+Input second integer:
+25
+Input third integer:
+30
+Sample Output
+Largest of three: 30
+Lowest of three: 15 
+*/
+namespace dotnet_exercises.w3resource.Basic
+{
+    public class Ex39 : IRunner
+    {
+        public void Run()
+        {
+            var arr = new List<int>();
+            Console.Write("Input first integer: ");
+            arr.Add(int.Parse(Console.ReadLine()));
+            Console.Write("Input second integer: ");
+            arr.Add(int.Parse(Console.ReadLine()));
+            Console.Write("Input thrid integer: ");
+            arr.Add(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Largest out of three: {arr.Max()}");
+            Console.WriteLine($"Lowest out of three: {arr.Min()}");
+
+        }
+    }
+}
+
