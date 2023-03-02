853d6784c2e24b0f733fa27a20a6f66e7f17e069
mateuszfranke <mateuszfranke@icloud.com>
2023-03-02 17:33:35 +0100

    exercise 40

---
+++ dotnet-exercises/w3resource/Basic/Ex40.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+Write a C# program to check the nearest value of 20 of two given integers and return 0 if two numbers are same.
+Test Data:
+Input first integer:
+15
+Input second integer:
+12
+Sample Output
+15
+*/
+namespace dotnet_exercises.w3resource.Basic
+{
+    public class Ex40 : IRunner
+    {
+        public void Run()
+        {
+            const int n = 20;
+            int answer = 0;
+            Console.Write("Input first integer: ");
+            int.TryParse(Console.ReadLine(), out var a);
+
+            Console.Write("Input second integer: ");
+            int.TryParse(Console.ReadLine(), out var b);
+
+            if (n - a < n - b)
+            {
+                answer = a;
+            }
+            else {
+
+                answer = b;
+            }
+          
+
+            Console.WriteLine($"{answer}");
+        }
+    }
+}
+
