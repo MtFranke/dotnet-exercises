5607532292beb7eac1e5e501837afab0c0c54d2a
mateuszfranke <mateuszfranke@icloud.com>
2023-03-06 08:20:56 +0100

    exercise 48

---
+++ dotnet-exercises/w3resource/Basic/Ex48.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+Write a C# program to check if the first element and the last element are equal of an array of integers and the length is 1 or more.
+Test Data:
+Array1: [1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1]
+Sample Output
+True 
+*/
+namespace dotnet_exercises.w3resource.Basic
+{
+    public class Ex48 : IRunner
+    {
+        public void Run()
+        {
+            const int searchedValue = 1;
+            int[] nums = new[] { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            var valueExist = false;
+
+            if (nums[0] == searchedValue && nums[^1] == searchedValue) {
+                valueExist = true;
+            }
+
+            Console.WriteLine(valueExist);
+
+        }
+    }
+}
