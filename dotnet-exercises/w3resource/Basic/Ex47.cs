100ed6b3a71293f76e3fb4e46f700f4494468cc2
mateuszfranke <mateuszfranke@icloud.com>
2023-03-06 08:14:14 +0100

    exercise 47

---
+++ dotnet-exercises/w3resource/Basic/Ex47.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+Write a C# program to compute the sum of all the elements of an array of integers.
+Test Data:
+Array1: [1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1]
+Sample Output
+Sum: 69 
+*/
+namespace dotnet_exercises.w3resource.Basic
+{
+    public class Ex47 : IRunner
+    {
+        public void Run()
+        {
+            int[] nums = new[] { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            var sum = nums.Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
