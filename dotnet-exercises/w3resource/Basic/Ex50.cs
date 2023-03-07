3fa30d503d2e1423b89d18951bcff1a24316e8e3
mateuszfranke <mateuszfranke@icloud.com>
2023-03-07 07:19:05 +0100

    exercise 50

---
+++ dotnet-exercises/w3resource/Basic/Ex50.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+Write a C# program to rotate an array (length 3) of integers in left direction.
+Test Data:
+Array1: [1, 2, 8]
+After rotating array becomes: [2, 8, 1] 
+*/
+namespace dotnet_exercises.w3resource.Basic
+{
+    public class Ex50 : IRunner
+    {
+        public void Run()
+        {
+            var nums = new[] { 1, 2, 8 };
+            Console.WriteLine("Before rotating array [{0}]", string.Join(", ", nums));
+
+            var q = new Queue<int>(nums);
+            var dequed = q.Dequeue();
+            q.Enqueue(dequed);
+
+            Console.WriteLine("After rotating array becomes: [{0}]", string.Join(", ", q));          
+        }
+    }
+}
+
