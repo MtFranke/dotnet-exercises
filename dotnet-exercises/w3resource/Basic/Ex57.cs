cffb6a20946a0bc2b5c543660d3c6b7c02e1c42c
mateuszfranke <mateuszfranke@icloud.com>
2023-03-13 08:03:03 +0100

    exercise 57

---
+++ dotnet-exercises/w3resource/Basic/Ex57.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics.Contracts;
+
+namespace dotnet_exercises.w3resource.Basic;
+
+//Write a C# program to find the pair of adjacent elements that has the highest product of an given array of integers.
+public class Ex57 : IRunner
+{
+    public void Run()
+    {
+        Console.WriteLine($"{DoAlgorithm(new [] { 1, -3, 4, -5, 1})}"); 
+        Console.WriteLine($"{DoAlgorithm(new [] { 1, 3, 4, 5, 2 })}"); 
+        Console.WriteLine($"{DoAlgorithm(new [] { 1 , 3, -4, 5, 2})}"); 
+        Console.WriteLine($"{DoAlgorithm(new [] { 1 , 0, -4, 0, 2})}"); 
+    }
+
+    [Pure]
+    private static int DoAlgorithm(int[] arr)
+    {
+        var maxProduct = arr[0] * arr[1];
+        for (var i = 0; i < arr.Length; i++)
+        {
+            if(i + 1 >= arr.Length) break;
+            var product = arr[i] * arr[i + 1];
+            maxProduct = Math.Max(product, maxProduct);
+        }
+
+        return maxProduct;
+    }
+}
